8c7941d6ab8d42d9193b1fbe7e30c599e4082ccc
Cauvery Raja S <cauveryraja25@gmail.com>
2023-02-05 19:14:39 +0530

    Add todos and rerender UI based on custom event

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+import TodoStore from './store/Todo.js'
+
 const Todos = new TodoStore()
 
 const App = {
     $: {
-        input: document.querySelector('[data-todo="new"]'),
+        formSection: document.querySelector('[data-todo="new"]'),
         list: document.querySelector('[data-todo="list"]'),
+        form: document.querySelector('form'),
     },
     showInput(show) {
         this.$.input.style.display = show ? 'block' : 'none';
@@ -12,19 +15,31 @@ const App = {
         App.$.list.style.display = show ? 'block' : 'none';
     },
     render() {
-        Todos.forEach(todo => {
+        Todos.todos.forEach(todo => {
             const tempStr = `<li>
                 <div class="todo">
                     <h3 class="todo-title">${todo.title}</h3>
                 </div>
             </li>`;
 
-            $.listElm.insertAdjacentHTML('beforeEnd', tempStr);
+            App.$.list.insertAdjacentHTML('beforeEnd', tempStr);
         });
 
         this.showList(true);
     },
+    bindEvents() {
+        this.$.form.addEventListener('submit', (event) => {
+            const formData = new FormData(this.$.form);
+
+            Todos.addItem(formData.get('title'));
+            event.preventDefault();
+        });
+
+        Todos.addEventListener('save', App.render)
+    },
     init() {
+        console.log('init...')
+        App.bindEvents();
         App.render();
     }
 }
--- js/store/Todo.js
+++ js/store/Todo.js
@@ -1,15 +1,27 @@
-class TodoStore {
+class TodoStore extends EventTarget {
     constructor() {
         super();
 
         this.todos = [];
     }
+
+    #save() {
+        this.dispatchEvent(new CustomEvent('save'));
+    }
     
-    addItem(todo) {
-        this.todos.push(todo);
+    addItem(title) {
+        this.todos.push({
+            title,
+            completed: false,
+            id: 'id_' + Date.now()
+        });
+
+        this.#save();
     }
 
     removeItem(todo) {
         this.todos = this.todos.filter(_ => _.id !== todo.id );
     }
 }
+
+export default TodoStore;
